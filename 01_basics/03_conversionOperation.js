101fa6d648f7c0b9a471faee3f2f1154c5c3cab0
MOHD KAIF <mohdkaif098900@gmail.com>
2026-07-21 03:03:03 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "hitesh"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueINNumber = Number(score)
+// console.log(typeof  valueINNumber);
+// console.log(valueINNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+let isLoggedIN = 1
+let booleanisLoggedIN = Boolean(isLoggedIN)
+// console.log(booleanisLoggedIN)
+
+// 1 => true; 0 = > false;
+// " " => false 
+// "kaif" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
