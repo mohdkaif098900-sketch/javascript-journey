1e36a03070f596339e530f1948f14206b03bbaad
MOHD KAIF <mohdkaif098900@gmail.com>
2026-07-22 01:36:58 +0530

    datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,41 @@
+// primitive
+
+// 7 types : String,Number,Boolean,Null,Undefined,Symbol,BigInt.
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIN = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('1234')
+const anotherId = Symbol('1234')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 328738738278372887392328n
+
+
+
+
+
+// (Reference)=>NONPRIMITIVE 
+// : Array,Object,function 
+
+const heros = ["Shaktiman", "Spiderman", "Saturu Gojo"];
+let myobj = {
+    name: "Kaif",
+    age: 22,
+
+}
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+console.log(typeof heros);
+
+
+
+    
+
