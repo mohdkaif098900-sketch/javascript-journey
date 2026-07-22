4eb3845ca97ec9557d95a9df7a11502b28bcdc3d
MOHD KAIF <mohdkaif098900@gmail.com>
2026-07-23 00:02:28 +0530

    string

---
+++ 01_basics/05_String.js
@@ -0,0 +1,46 @@
+const name = " kaif "
+const repoCount = 50
+// console.log(name + repoCount + " Value");
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('mohdkaif-zz')
+// console.log(gameName[0]);
+// console.log(gameName.__proto___);
+
+
+
+// console.log(gameName. length);
+// console.log(gameName. toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('i'));
+
+const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+
+const newStringOne = "   mohdkaif      "
+// console.log(newStringOne);
+// console.log(newString.trim());
+
+const url = "https://mohdkaifff.com/kaif%20mohd"
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+
